2b8576b232df6a59e09ffb7c85c8ab7ace586c47
Gilles Crofils <gilles@secondbureau.com>
2016-08-17 10:31:15 +0800

    bug : Custom field format

--- app/models/concerns/camaleon_cms/custom_fields_read.rb
+++ app/models/concerns/camaleon_cms/custom_fields_read.rb
@@ -188,8 +188,9 @@ module CamaleonCms::CustomFieldsRead extend ActiveSupport::Concern
   def set_field_values(datas = {})
     if datas.present?
       self.field_values.delete_all
-      datas.each do |index, fields_data|
-        fields_data.each do |field_key, values|
+      #datas.each do |index, fields_data|
+      #  fields_data.each do |field_key, values|
+      datas.each do |index, values|
           if values[:values].present?
             order_value = -1
             ((values[:values].is_a?(Hash) || values[:values].is_a?(ActionController::Parameters)) ? values[:values].values : values[:values]).each do |value|
@@ -197,7 +198,7 @@ module CamaleonCms::CustomFieldsRead extend ActiveSupport::Concern
             end
           end
         end
-      end
+        #end
     end
   end
 
